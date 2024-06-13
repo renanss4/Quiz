ca0b6c6bded880815f8b9a1e0a1f25b944f3c662
Renan <renansantos.silva04@outlook.com>
2024-06-13 11:49:28 -0300

    refactor: translated to en-us

--- backend/middlewares/authenticate.js
+++ backend/middlewares/authenticate.js
@@ -4,13 +4,13 @@ export function checkToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (!token) {
-    return res.status(401).json({ msg: "Token não encontrado!" });
+    return res.status(401).json({ msg: "Token not found!" });
   }
   try {
     const secret = process.env.JWT_SECRET;
     jwt.verify(token, secret, (error, payload) => {
       if (error) {
-        return res.status(403).json({ msg: "Token inválido!" });
+        return res.status(403).json({ msg: "Invalid token!" });
       }
       req.payload = payload;
       //   console.log("Payload: ", payload);
@@ -18,6 +18,6 @@ export function checkToken(req, res, next) {
     });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ msg: "Erro ao verificar token!" });
+    return res.status(500).json({ msg: "Error verifying token!" });
   }
 }
