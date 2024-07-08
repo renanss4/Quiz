e5d155ce359fe771d3427b222ae5362617420fa9
Renan <renansantos.silva04@outlook.com>
2024-07-08 13:33:31 -0300

    feat: using the middleware on server

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cors from "cors"; // Imports CORS to allow requests from different origin
 import { config } from "dotenv"; // Imports dotenv to load environment variables from a .env file
 import { connectToDatabase } from "./database/connect.js"; // Imports the function to connect to the database
 import routes from "./routes/routes.js"; // Imports the route configuration
+import { errorHandler } from "./middlewares/errorHandler.js"; // Imports the error handler middleware
 
 // Loads environment variables from the .env file
 config();
@@ -16,6 +17,7 @@ const server = express(); // Creates an instance of the Express server
 server.use(bodyParser.json()); // Configures body-parser to parse request bodies as JSON
 server.use(cors()); // Enables CORS to allow requests from other origins
 server.use("/", routes); // Sets up the route configuration
+server.use(errorHandler); // Sets up the error handler middleware
 
 const port = process.env.PORT || 8080; // Defines the port the server will run on, using the port defined in the environment variables or 8080 as default
 
