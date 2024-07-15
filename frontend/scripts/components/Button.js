ebb49a8a8836d653909929e80a1ef516312e0b54
Renan Silva <renansantos.silva04@outlook.com>
2024-07-15 17:46:29 -0300

    Continuing the study in componentization

--- frontend/scripts/components/Button.js
+++ frontend/scripts/components/Button.js
@@ -1,10 +1,34 @@
-import { createElement } from "../configs/createElement.js";
+function Button({ title = "Button", icon = false, type = "default", link = null, onClick }) {
+  const button = link ? document.createElement("a") : document.createElement("button");
 
-export function Button({ type, onClick, text }) {
-  return createElement(
-    "button",
-    { class: "btn-login", type: type, onclick: onClick },
-    text
-  );
-  // document.contentType = type
+  button.textContent = icon ? `${icon} ${title}` : title;
+
+  if (link) {
+    button.href = link;
+    button.onclick = onClick ? (e) => { e.preventDefault(); onClick(); } : null;
+  } else {
+    button.onclick = onClick;
+  }
+
+  const baseClass = "button";
+  let typeClass;
+
+  switch (type) {
+    case "outline":
+      typeClass = "button-outline";
+      break;
+    case "destructive":
+      typeClass = "button-default-destructive";
+      break;
+    case "destructive-outline":
+      typeClass = "button-outline-destructive";
+      break;
+    default:
+      typeClass = "button-default";
+      break;
+  }
+
+  button.classList.add(baseClass, typeClass);
+
+  return button;
 }
