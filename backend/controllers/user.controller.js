66076a90d6745c8583dbe12acb6fa802fc284012
Renan Silva <renansantos.silva04@outlook.com>
2024-07-15 21:03:34 -0300

    removing try catch from controller methods and using middleware in routes to deal with the error

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -78,163 +78,128 @@ class UserController {
   }
 
   async readAdmins(req, res) {
-    try {
-      // Checks if the user logged in is an admin
-      const isAdmin = req.payload.role;
-      if (isAdmin !== "admin") {
-        return res
-          .status(403)
-          .send({ msg: "You don't have permission for this funcionality" });
-      }
-
-      // Finds all admins in the database
-      const admins = await usersModel.find({ role: "admin" }, "-__v");
-      if (admins.length === 0 || !admins)
-        return res.status(404).send("No admins found");
-
-      // Returns a 200 status with the found admins
-      return res.status(200).json(admins);
-    } catch (error) {
-      console.log({ Error: `${error.message}` });
-      return res.status(500).json({ Error: `${error.message}` });
+    // Checks if the user logged in is an admin
+    const isAdmin = req.payload.role;
+    if (isAdmin !== "admin") {
+      return res
+        .status(403)
+        .send({ msg: "You don't have permission for this funcionality" });
     }
+
+    // Finds all admins in the database
+    const admins = await usersModel.find({ role: "admin" }, "-__v");
+    if (admins.length === 0 || !admins)
+      return res.status(404).send("No admins found");
+
+    // Returns a 200 status with the found admins
+    return res.status(200).json(admins);
   }
 
   async readStudents(req, res) {
-    try {
-      // Finds all students in the database
-      const students = await usersModel.find({ role: "student" }, "-__v");
-      if (students.length === 0 || !students)
-        return res.status(404).send("No students found");
+    // Finds all students in the database
+    const students = await usersModel.find({ role: "student" }, "-__v");
+    if (students.length === 0 || !students)
+      return res.status(404).send("No students found");
 
-      // Returns a 200 status with the found students
-      return res.status(200).json(students);
-    } catch (error) {
-      console.log({ Error: `${error.message}` });
-      return res.status(500).json({ Error: `${error.message}` });
-    }
+    // Returns a 200 status with the found students
+    return res.status(200).json(students);
   }
 
   async readTeachers(req, res) {
-    try {
-      // Finds all teachers in the database
-      const teachers = await usersModel.find({ role: "teacher" }, "-__v");
-      if (teachers.length === 0 || !teachers)
-        return res.status(404).send("No teacher found");
+    // Finds all teachers in the database
+    const teachers = await usersModel.find({ role: "teacher" }, "-__v");
+    if (teachers.length === 0 || !teachers)
+      return res.status(404).send("No teacher found");
 
-      // Returns a 200 status with the found teachers
-      return res.status(200).json(teachers);
-    } catch (error) {
-      console.log({ Error: `${error.message}` });
-      return res.status(500).json({ Error: `${error.message}` });
-    }
+    // Returns a 200 status with the found teachers
+    return res.status(200).json(teachers);
   }
 
   async readUserById(req, res) {
-    try {
-      // Checks if the user logged in is an admin
-      // const isAdmin = req.payload.role;
-      // if (isAdmin !== "admin") {
-      //   return res
-      //     .status(403)
-      //     .send({ msg: "You don't have permission for this funcionality" });
-      // }
+    // Checks if the user logged in is an admin
+    // const isAdmin = req.payload.role;
+    // if (isAdmin !== "admin") {
+    //   return res
+    //     .status(403)
+    //     .send({ msg: "You don't have permission for this funcionality" });
+    // }
 
-      // Finds only a user in the database
-      const id = req.params.id;
+    // Finds only a user in the database
+    const id = req.params.id;
 
-      // checks if id is valid
-      if (!id || !id.match(/^[0-9a-fA-F]{24}$/)) {
-        return res.status(400).send("Invalid ID");
-      }
-
-      const user = await usersModel.findById(id, "-password -__v"); // Returns the user without the password and the version key
-      if (!user) {
-        return res.status(404).send("Not found or does not exist"); // Returns a 404 status if the user doesn't exist
-      }
-
-      // Returns a 200 status with the found user
-      return res.status(200).json(user); // Returns a 200 status with the found user
-    } catch (error) {
-      console.log({ Error: `${error.message}` });
-      return res.status(500).json({ Error: `${error.message}` }); // Returns a 500 status with an error message if an error occurs
+    // checks if id is valid
+    if (!id || !id.match(/^[0-9a-fA-F]{24}$/)) {
+      return res.status(400).send("Invalid ID");
     }
+
+    const user = await usersModel.findById(id, "-password -__v"); // Returns the user without the password and the version key
+    if (!user) {
+      return res.status(404).send("Not found or does not exist"); // Returns a 404 status if the user doesn't exist
+    }
+
+    // Returns a 200 status with the found user
+    return res.status(200).json(user); // Returns a 200 status with the found user
   }
 
   async readUsers(req, res) {
-    try {
-      // Checks if the user logged in is an admin
-      const isAdmin = req.payload.role;
-      if (isAdmin !== "admin") {
-        return res
-          .status(403)
-          .send({ msg: "You don't have permission for this funcionality" });
-      }
-
-      // Finds all users in the database
-      const users = await usersModel.find({}, "-password -__v");
-      if (users.length === 0 || !users) {
-        return res.status(404).send("No users found"); // Returns a 404 status if no users are found
-      }
-
-      // Returns a 200 status with the found users
-      return res.status(200).json(users); // Returns a 200 status with the found users
-    } catch (error) {
-      console.log({ Error: `${error.message}` });
-      return res.status(500).json({ Error: `${error.message}` }); // Returns a 500 status with an error message if an error occurs
+    // Checks if the user logged in is an admin
+    const isAdmin = req.payload.role;
+    if (isAdmin !== "admin") {
+      return res
+        .status(403)
+        .send({ msg: "You don't have permission for this funcionality" });
     }
+
+    // Finds all users in the database
+    const users = await usersModel.find({}, "-password -__v");
+    if (users.length === 0 || !users) {
+      return res.status(404).send("No users found"); // Returns a 404 status if no users are found
+    }
+
+    // Returns a 200 status with the found users
+    return res.status(200).json(users); // Returns a 200 status with the found users
   }
 
   async updateUser(req, res) {
-    try {
-      // Checks if the user logged in is an admin
-      const isAdmin = req.payload.role;
-      if (isAdmin !== "admin") {
-        return res
-          .status(403)
-          .send({ msg: "You don't have permission for this funcionality" });
-      }
-
-      const id = req.params.id; // Retrieves the id parameter from the request
-
-      // Updates the user with the provided id using the data from the request body
-      const updatedUser = await usersModel.findByIdAndUpdate(id, req.body, {
-        new: true,
-      });
-
+    // Checks if the user logged in is an admin
+    const isAdmin = req.payload.role;
+    if (isAdmin !== "admin") {
       return res
-        .status(201)
-        .json(updatedUser)
-        .send({ msg: "Succesfully updated" }); // Returns a 200 status with the updated user
-    } catch (error) {
-      console.log({ Error: `${error.message}` });
-      return res.status(500).json({ Error: `${error.message}` }); // Returns a 500 status with an error message if an error occurs
+        .status(403)
+        .send({ msg: "You don't have permission for this funcionality" });
     }
+
+    const id = req.params.id; // Retrieves the id parameter from the request
+
+    // Updates the user with the provided id using the data from the request body
+    const updatedUser = await usersModel.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+
+    return res
+      .status(201)
+      .json(updatedUser)
+      .send({ msg: "Succesfully updated" }); // Returns a 200 status with the updated user
   }
 
   async deleteUser(req, res) {
-    try {
-      // Checks if the user logged in is an admin
-      const isAdmin = req.payload.role;
-      if (isAdmin !== "admin") {
-        return res
-          .status(403)
-          .send({ msg: "You don't have permission for this funcionality" });
-      }
-
-      const id = req.params.id; // Retrieves the id parameter from the request
-
-      // Deletes the user with the provided id
-      const deletedUser = await usersModel.findByIdAndDelete(id);
-
+    // Checks if the user logged in is an admin
+    const isAdmin = req.payload.role;
+    if (isAdmin !== "admin") {
       return res
-        .status(200)
-        .json(deletedUser)
-        .send({ msg: "Successfully deleted" }); // Returns a 200 status with the deleted user
-    } catch (error) {
-      console.log({ Error: `${error.message}` });
-      return res.status(500).json({ Error: `${error.message}` }); // Returns a 500 status with an error message if an error occurs
+        .status(403)
+        .send({ msg: "You don't have permission for this funcionality" });
     }
+
+    const id = req.params.id; // Retrieves the id parameter from the request
+
+    // Deletes the user with the provided id
+    const deletedUser = await usersModel.findByIdAndDelete(id);
+
+    return res
+      .status(200)
+      .json(deletedUser)
+      .send({ msg: "Successfully deleted" }); // Returns a 200 status with the deleted user
   }
 }
 
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,32 +1,19 @@
 import { Router } from "express";
-import { UserController } from "../controllers/user.controller.js";
+import UserController from "../controllers/user.controller.js";
 import { tryCatch } from "../utils/tryCatch.js";
 
 const userRoute = Router();
 
-// PRIVATE ROUTES FOR ALL USERS
-userRoute.get("/students", UserController.readStudents);
-userRoute.get("/teachers", UserController.readTeachers);
-
-// PRIVATE ROUTES FOR ADMINS
-// POST routes
 userRoute.post("/", tryCatch(UserController.createUser));
-// userRoute.post("/token", UserController.tokenUser);
 
-// GET routes
-userRoute.get("/search/:id?", UserController.readUserById); // query params
-userRoute.get("/admins", UserController.readAdmins);
-userRoute.get("/", UserController.readUsers);
-// userRoute.get("/role", UserController.roleUser);
+userRoute.get("/search/:id?", tryCatch(UserController.readUserById)); // query params
+userRoute.get("/admins", tryCatch(UserController.readAdmins));
+userRoute.get("/students", tryCatch(UserController.readStudents));
+userRoute.get("/teachers", tryCatch(UserController.readTeachers));
+userRoute.get("/", tryCatch(UserController.readUsers));
 
-// PATCH routes
-userRoute.patch("/:id", UserController.updateUser);
+userRoute.patch("/:id", tryCatch(UserController.updateUser));
 
-// DELETE routes
-userRoute.delete("/:id", UserController.deleteUser);
-
-// PRIVATE ROUTES FOR STUDENTS
-
-// PRIVATE ROUTES FOR TEACHERS
+userRoute.delete("/:id", tryCatch(UserController.deleteUser));
 
 export default userRoute;
