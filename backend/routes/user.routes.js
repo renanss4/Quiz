2059ec903c27c2a61e225dcaa8f7f9a013784841
Renan Silva <renansantos.silva04@outlook.com>
2024-07-16 19:17:33 -0300

    added comments to explain the search route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -6,6 +6,16 @@ const userRoute = Router();
 
 userRoute.post("/", tryCatch(UserController.createUser));
 
+
+/* 
+    works with query params
+
+    1. /search finds all users
+    2. e.g. /search?id=123
+    3. e.g. /search?role=admin
+    4. e.g. /search?enrollment=123456
+    5. e.g. /search?role=admin&id=123&enrollment=123456
+*/
 userRoute.get("/search", tryCatch(UserController.readUsers));
 
 userRoute.patch("/:id", tryCatch(UserController.updateUser));
