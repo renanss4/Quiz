e9cee211a6f847e53bda5ac6d0efeaae6082cf30
Renan <renansantos.silva04@outlook.com>
2024-07-17 10:12:43 -0300

    Calling components better

--- frontend/scripts/teste.js
+++ frontend/scripts/teste.js
@@ -7,140 +7,57 @@ function teste() {
 
 document.addEventListener("DOMContentLoaded", () => {
   const app = document.getElementById("app");
-  const button1 = Button({
-    type: "default",
-    size: "full",
-    text: "Clique aqui",
-    // imgSrc: "../assets/create.svg",
-    onClick: teste,
-  });
 
-  const button2 = Button({
-    type: "default",
-    size: "medium",
-    text: "Clique aqui",
-    // imgSrc: "../assets/create.svg",
-    onClick: teste,
-  });
-
-  const button3 = Button({
-    type: "default",
-    size: "small",
-    text: "Clique aqui",
-    // imgSrc: "../assets/create.svg",
-    onClick: teste,
-  });
-
-  const button4 = Button({
-    type: "outline",
-    size: "large",
-    text: "Clique aqui",
-    // imgSrc: "../assets/create.svg",
-    onClick: teste,
-  });
-
-  const button5 = Button({
-    type: "outline",
-    size: "medium",
-    text: "Clique aqui",
-    // imgSrc: "../assets/create.svg",
-    onClick: teste,
-  });
-
-  const button6 = Button({
-    type: "outline",
-    size: "small",
-    text: "Clique aqui",
-    // imgSrc: "../assets/create.svg",
-    onClick: teste,
-  });
-
-  const button7 = Button({
-    type: "destructive",
-    size: "large",
-    text: "Clique aqui",
-    // imgSrc: "../assets/create.svg",
-    onClick: teste,
-  });
-
-  const button8 = Button({
-    type: "destructive",
-    size: "medium",
-    text: "Clique aqui",
-    // imgSrc: "../assets/create.svg",
-    onClick: teste,
-  });
-
-  const button9 = Button({
-    type: "destructive",
-    size: "small",
-    text: "Clique aqui",
-    // imgSrc: "../assets/create.svg",
-    onClick: teste,
-  });
-
-  const button10 = Button({
-    type: "destructive-outline",
-    size: "large",
-    text: "Clique aqui",
-    // imgSrc: "../assets/create.svg",
-    onClick: teste,
-  });
-
-  const button11 = Button({
-    type: "destructive-outline",
-    size: "medium",
-    text: "Clique aqui",
-    // imgSrc: "../assets/create.svg",
-    onClick: teste,
-  });
-
-  const button12 = Button({
-    type: "destructive-outline",
-    size: "small",
-    text: "Clique aqui",
-    // imgSrc: "../assets/create.svg",
-    onClick: teste,
-  });
-
-  const button13 = Button({
-    type: "default",
-    size: "small",
-    text: "Clique aqui",
-    imgSrc: "../assets/create.svg",
-    onClick: teste,
-  });
-
-  const button14 = Button({
-    type: "link",
-    text: "Link para o Google",
-    link: "https://google.com",
-  });
-
-  const button15 = Button({
-    type: "link-sidebar",
-    text: "Botão de sidebar",
-    link: "https://google.com",
-  });
-
-  const input1 = Input({
-    type: "select",
-    placeholder: "Digite algo",
-    value: "",
-    onChange: (e) => {
-      console.log(e.target.value);
+  // Adiciona botões
+  const buttons = [
+    { type: "default", size: "full", text: "Clique aqui" },
+    { type: "default", size: "medium", text: "Clique aqui" },
+    { type: "default", size: "small", text: "Clique aqui" },
+    { type: "outline", size: "large", text: "Clique aqui" },
+    { type: "outline", size: "medium", text: "Clique aqui" },
+    { type: "outline", size: "small", text: "Clique aqui" },
+    { type: "destructive", size: "large", text: "Clique aqui" },
+    { type: "destructive", size: "medium", text: "Clique aqui" },
+    { type: "destructive", size: "small", text: "Clique aqui" },
+    { type: "destructive-outline", size: "large", text: "Clique aqui" },
+    { type: "destructive-outline", size: "medium", text: "Clique aqui" },
+    { type: "destructive-outline", size: "small", text: "Clique aqui" },
+    {
+      type: "default",
+      size: "small",
+      text: "Clique aqui",
+      imgSrc: "../assets/create.svg",
     },
-  });
-  const input2 = Input({
-    type: "number",
-    placeholder: "Digite algo",
-    value: "",
-    onChange: (e) => {
-      console.log(e.target.value);
+    { type: "link", text: "Link para o Google", link: "https://google.com" },
+    {
+      type: "link-sidebar",
+      text: "Botão de sidebar",
+      link: "https://google.com",
     },
-  });
+  ];
 
-  app.appendChild(input1);
-  app.appendChild(input2);
-  app.appendChild(button1);
+  // buttons.forEach((buttonConfig) => {
+  //   const button = Button({
+  //     ...buttonConfig,
+  //     onClick: teste,
+  //   });
+  //   app.appendChild(button);
+  // });
+
+  // Adiciona inputs
+  const inputs = [
+    { type: "email", placeholder: "Digite algo", name: "email" },
+    { type: "password", placeholder: "Digite algo", name: "password" },
+    { type: "text", placeholder: "Digite algo", name: "text" },
+  ];
+
+  // inputs.forEach((inputConfig) => {
+  //   const input = Input({
+  //     ...inputConfig,
+  //     onChange: (e) => {
+  //       console.log(e.target);
+  //     },
+  //   });
+  //   app.appendChild(input);
+  // });
 });
