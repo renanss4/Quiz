ef668acf3cae6443e58c1d321513b7f758bf33b9
Renan <renansantos.silva04@outlook.com>
2024-07-26 10:57:24 -0300

    Refactored Toaster

--- frontend/components/Toaster/Toaster.js
+++ frontend/components/Toaster/Toaster.js
@@ -18,22 +18,20 @@ export function Toaster({
   const toasterClass = stylesByType[type] || stylesByType["success"];
   toaster.classList.add(toasterClass);
 
-  // Cria o ícone se a propriedade iconSrc for definida
-  const iconElement = document.createElement("div");
-  iconElement.classList.add("toaster-icon");
+  const topElement = document.createElement("div");
+  const titleDiv = document.createElement("div");
+  titleDiv.classList.add("title-div");
+  topElement.classList.add("toaster-top");
   if (iconSrc) {
     const img = document.createElement("img");
     img.setAttribute("src", iconSrc);
-    iconElement.appendChild(img);
+    titleDiv.appendChild(img);
   }
-
-  // Cria o conteúdo do toaster
-  const contentElement = document.createElement("div");
-  contentElement.classList.add("toaster-content");
 
   // Cria o título
   const titleElement = document.createElement("h2");
   titleElement.innerHTML = title;
+  titleDiv.appendChild(titleElement);
 
   // Cria a mensagem
   const messageElement = document.createElement("p");
@@ -45,14 +43,14 @@ export function Toaster({
   closeIcon.classList.add("toaster-close-icon");
   closeIcon.addEventListener("click", onClose);
 
-  // Adiciona os elementos ao contentElement
-  contentElement.appendChild(titleElement);
-  contentElement.appendChild(messageElement);
+  topElement.appendChild(titleDiv);
 
   // Adiciona os elementos ao toaster
-  toaster.appendChild(iconElement);
-  toaster.appendChild(contentElement);
-  toaster.appendChild(closeIcon);
+  toaster.appendChild(topElement);
+
+  // Adiciona os elementos ao contentElement
+  topElement.appendChild(closeIcon);
+  toaster.appendChild(messageElement);
 
   return toaster;
 }
