d4e7c6f4b7571e425d19a1ee7270c2f6524c5b81
Renan <renansantos.silva04@outlook.com>
2024-08-02 11:09:15 -0300

    refactor: refactoring the tokenManager

--- frontend/scripts/configs/tokenManager.js
+++ frontend/scripts/configs/tokenManager.js
@@ -14,13 +14,34 @@ function getToken() {
 }
 
 // Check if the token is present in the localStorage
-function checkAuthentication() {
+async function checkAuthenticationByToken() {
   const token = getToken();
   if (!token) {
     // Redirect to the login page if the token does not exist
     window.location.href = "login.html";
+  } else {
+    //  Decode the token and check expiration
+    const decoded = decodeToken(token);
+    const currentTime = Math.floor(Date.now() / 1000);
+    if (decoded.exp < currentTime) {
+      removeToken();
+      window.location.href = "login.html";
+    }
   }
 }
 
+// Decode the token
+function decodeToken(token) {
+  const payload = token.split(".")[1];
+  const decoded = atob(payload);
+  return JSON.parse(decoded);
+}
+
 // Export the functions
-export { getToken, setToken, removeToken, checkAuthentication };
+export {
+  getToken,
+  setToken,
+  removeToken,
+  checkAuthenticationByToken,
+  decodeToken,
+};
