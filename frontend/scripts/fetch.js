6d2c227a50724040c116367aca594bab17582ab2
Renan <renansantos.silva04@outlook.com>
2024-08-20 11:42:42 -0300

    added a new function to add a teacher to a subject

--- frontend/scripts/fetch.js
+++ frontend/scripts/fetch.js
@@ -107,6 +107,40 @@ export async function fetchTeachers() {
   return fetchUsers({ role: "teacher" });
 }
 
+// Function to add a teacher to a subject
+export async function addTeacherToSubject(subject_id, teacher_id) {
+  await checkAuthenticationByToken();
+
+  const token = getToken();
+  if (!token) {
+    throw new Error("No token found!");
+  }
+
+  try {
+    const response = await fetch(`${URL}/subject/${subject_id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ teacher_id }),
+    });
+
+    if (response.status === 204) {
+      return;
+    }
+
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.message || "Error adding teacher to subject");
+    }
+
+    return data;
+  } catch (error) {
+    throw new Error("Network error: " + error.message);
+  }
+}
+
 // Function to fetch all students
 export async function fetchStudents() {
   return fetchUsers({ role: "student" });
