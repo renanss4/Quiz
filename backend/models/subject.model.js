9eed1d262b9509a48c77e05204971539e159151e
Renan <renansantos.silva04@outlook.com>
2024-08-27 09:20:14 -0300

    feat: edit to contain subdoc about quiz

--- backend/models/subject.model.js
+++ backend/models/subject.model.js
@@ -1,33 +1,37 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
 
-const subjectSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: [3, "The name must have at least 3 characters!"],
+const subjectSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: [3, "The name must have at least 3 characters!"],
+    },
+    teacher_id: {
+      type: Schema.Types.ObjectId,
+      ref: "Users",
+    },
+    quizzes: [
+      {
+        quiz_id: {
+          type: Schema.Types.ObjectId,
+          ref: "Quizzes",
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+          trim: true,
+          minlength: [3, "The name must have at least 3 characters!"],
+        },
+      },
+    ],
   },
-  teacher_id: {
-    type: Schema.Types.ObjectId,
-    ref: "Users",
-  },
-  // quizzes: [{
-  //   quiz_id: {
-  //     type: Schema.Types.ObjectId,
-  //     ref: "Quizzes",
-  //     required: true,
-  //   },
-  //   name: {
-  //     type: String,
-  //     required: true,
-  //     trim: true,
-  //     minlength: [3, "The name must have at least 3 characters!"],
-  //   }
-  // }],
-},
-{
-  timestamps: false,
-});
+  {
+    timestamps: false,
+  }
+);
 
 export const subjectsModel = mongoose.model("Subjects", subjectSchema);
